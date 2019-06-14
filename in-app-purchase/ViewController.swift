e8d5cc1f2c4c292604ecfa697c2f547b3e39eea7
takehiro <takehiro.watanabe@globis.co.jp>
2019-06-14 13:48:51 +0900

    Initial Commit

---
+++ in-app-purchase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  in-app-purchase
+//
+//  Created by Watanabe Takehiro on 2019/06/14.
+//  Copyright © 2019 Watanabe Takehiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
